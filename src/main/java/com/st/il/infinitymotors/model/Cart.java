8392b433a72b1f2333a70f24d3ae5bf27dd4746f
Al Amine <amine.anaji@gmail.com>
2019-06-05 22:02:18 +0100

    base skeleton and models

---
+++ src/main/java/com/st/il/infinitymotors/model/Cart.java
@@ -0,0 +1,27 @@
+package com.st.il.infinitymotors.model;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+/**
+ * A car in a the database.
+ *
+ * @author Tonny Huang
+ * @author Vien Yeung
+ * @author Al Amine
+ */
+@Entity
+@Table(name = "tbl_cart")
+public class Cart {
+
+}
