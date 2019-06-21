4310840c2a2a8df86639c7f0b1b95ef47ec5ae56
Tonny Huang <tonny.huang0724@gmail.com>
2019-06-21 15:24:55 -0400

    Dao Changes

--- src/main/java/com/st/il/infinitymotors/adminapp/dao/OrderDao.java
+++ src/main/java/com/st/il/infinitymotors/adminapp/dao/OrderDao.java
@@ -1,11 +1,20 @@
 package com.st.il.infinitymotors.adminapp.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.st.il.infinitymotors.adminapp.model.Order;
 
 @Repository
 public interface OrderDao extends JpaRepository<Order, Integer> {
-
+	
+	@Query(
+	        value = "SELECT * FROM tbl_order NATURAL JOIN tbl_user u WHERE u.userId = :userId", 
+	        nativeQuery=true
+	    )
+	public List<Order> findAllByUser(@Param("userId") Integer userId);
 }
